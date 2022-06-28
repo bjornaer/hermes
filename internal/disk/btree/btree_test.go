144363d78af98c400772f113992afc4370852e33
bjornaer <maxemijo@gmail.com>
2022-06-28 12:13:09 +0100

    refactor btree test with testify

--- internal/disk/btree/btree_test.go
+++ internal/disk/btree/btree_test.go
@@ -7,70 +7,113 @@ import (
 
 	"github.com/bjornaer/hermes/internal/disk/btree"
 	"github.com/bjornaer/hermes/internal/disk/pair"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-func clearDB() string {
+type UnitTestSuite struct {
+	suite.Suite
+	totalElements int
+	path          string
+	tree          *btree.Btree
+}
+
+func (s *UnitTestSuite) SetupTest() {
 	path := "./db/test.db"
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		os.Mkdir("./db", os.ModePerm)
 	}
-	if _, err := os.Stat(path); err == nil {
+	s.path = "./db/"
+	tree, err := btree.InitializeBtree(path)
+	if err != nil {
+		s.T().Error(err)
+	}
+	s.tree = tree
+
+}
+
+func (s *UnitTestSuite) BeforeTest(suiteName, testName string) {
+	for i := 1; i <= s.totalElements; i++ {
+		key := fmt.Sprintf("key-%d", i)
+		value := fmt.Sprintf("value-%d", i)
+		s.tree.Insert(pair.NewPair(key, value))
+	}
+}
+
+func (s *UnitTestSuite) AfterTest(suiteName, testName string) {
+	if _, err := os.Stat(s.path); err == nil {
 		// path/to/whatever exists
-		err := os.Remove(path)
+		err := os.RemoveAll(s.path)
 		if err != nil {
 			panic(err)
 		}
 	}
-	return path
 }
 
-func TestBtreeInsert(t *testing.T) {
-	tree, err := btree.InitializeBtree(clearDB())
-	if err != nil {
-		t.Error(err)
-	}
+func BtreeInsert(s *UnitTestSuite) {
 	for i := 1; i <= 100; i++ {
 		key := fmt.Sprintf("key-%d", i)
 		value := fmt.Sprintf("value-%d", i)
-		if i == 230 {
-			println("Inserted 229 elements")
-		}
-		tree.Insert(pair.NewPair(key, value))
+		err := s.tree.Insert(pair.NewPair(key, value))
+		assert.Nil(s.T(), err)
 	}
 	// tree.root.PrintTree()
 }
 
-func TestBtreeGet(t *testing.T) {
-	tree, err := btree.InitializeBtree(clearDB())
-	if err != nil {
-		t.Error(err)
-	}
-	totalElements := 500
-	for i := 1; i <= totalElements; i++ {
+func BtreeGet(s *UnitTestSuite) {
+	for i := 1; i <= s.totalElements; i++ {
 		key := fmt.Sprintf("key-%d", i)
-		value := fmt.Sprintf("value-%d", i)
-		tree.Insert(pair.NewPair(key, value))
+		expected := fmt.Sprintf("value-%d", i)
+		value, _, found, err := s.tree.Get(key)
+		if err != nil {
+			s.T().Error(err)
+		}
+		assert.True(s.T(), found)
+		assert.Equal(s.T(), expected, value)
+	}
+}
+
+func BtreeGetInexistent(s *UnitTestSuite) {
+	for i := s.totalElements + 1; i <= s.totalElements+1+1000; i++ {
+		key := fmt.Sprintf("key-%d", i)
+		_, _, found, err := s.tree.Get(key)
+		if err != nil {
+			s.T().Error(err)
+		}
+		assert.False(s.T(), found, "values should not be found")
+	}
+}
+
+func (s *UnitTestSuite) Test_TableTest() {
+
+	type testCase struct {
+		name   string
+		treeFn func(s *UnitTestSuite)
 	}
 
-	for i := 1; i <= totalElements; i++ {
-		key := fmt.Sprintf("key-%d", i)
-		value, _, found, err := tree.Get(key)
-		if err != nil {
-			t.Error(err)
-		}
-		if !found || value == "" {
-			t.Error("Value should be found ", key)
-		}
+	testCases := []testCase{
+		{
+			name:   "Insert Multiple",
+			treeFn: BtreeInsert,
+		},
+		{
+			name:   "Get Each Value",
+			treeFn: BtreeInsert,
+		},
+		{
+			name:   "Get Non Existent Value",
+			treeFn: BtreeInsert,
+		},
 	}
 
-	for i := totalElements + 1; i <= totalElements+1+1000; i++ {
-		key := fmt.Sprintf("key-%d", i)
-		_, _, found, err := tree.Get(key)
-		if err != nil {
-			t.Error(err)
-		}
-		if found {
-			t.Error("Value should not be found")
-		}
+	for _, testCase := range testCases {
+
+		s.Run(testCase.name, func() {
+			testCase.treeFn(s)
+		})
 	}
+}
+
+func TestUnitTestSuite(t *testing.T) {
+	suite.Run(t, new(UnitTestSuite))
 }
